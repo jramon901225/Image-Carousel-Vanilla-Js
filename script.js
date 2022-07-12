c4530c35934ba058e71a6500ec28e4563cfad3d1
Jesus Manuel Ramon Colorado <jramon901225@gmail.com>
2022-07-12 16:38:42 -0500

    Image Carousel with Vainila JS

---
+++ script.js
@@ -0,0 +1,24 @@
+let index = 0;
+
+show_image(index);
+
+function show_image(i) {
+  index += i;
+
+  const images = document.getElementsByClassName("image");
+  const dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < images.length; i++) images[i].style.display = "none";
+
+  for (i = 0; i < dots.length; i++)
+    dots[i].className = dots[i].className.replace("active", "");
+
+  if (index > images.length - 1) index = 0;
+
+  if (index < 0) index = images.length - 1;
+
+  console.log(index);
+
+  images[index].style.display = "block";
+  dots[index].className += " active";
+}
